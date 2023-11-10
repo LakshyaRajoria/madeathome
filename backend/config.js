afa63a9cfe70388b6d5fb0cd2b05902ffa07d06a
LakshyaRajoria <lakshyarajoria@gmail.com>
2023-11-10 01:14:13 -0500

    fixed bugs in rendering

--- backend/config.js
+++ backend/config.js
@@ -2,4 +2,4 @@ export const PORT = 3000;
 
 
 export const mongoDBURL = 
-    'mongodb+srv://lakshyarajoria:Lakshya1@madeathome.sqohsvv.mongodb.net/madeathome-collection-1?retryWrites=true&w=majority'
+    'mongodb+srv://lakshyarajoria:mongotest@madeathome.sqohsvv.mongodb.net/madeathome-collection-1?retryWrites=true&w=majority'
--- backend/models/menuItems.js
+++ backend/models/menuItems.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const menuItemSchema = new mongoose.Schema({
-    category: {type: String, required: true, unique: true}, 
-    cuisine: {type: String, required: true, unique: true}, 
+    category: {type: String, required: true}, 
+    cuisine: {type: String, required: true}, 
     name: {type: String, required: true, unique: true}, 
     description: {type: String, required: true, unique: true}, 
     price: {type: Number, required: true}, 
