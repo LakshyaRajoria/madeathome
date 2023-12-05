56c71abbf4be02fd9fd7c7829bddb0a076b19eb0
LakshyaRajoria <62789062+LakshyaRajoria@users.noreply.github.com>
2023-12-06 05:04:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: mongoDBURL }),
-    cookie: { secure: NODE_ENV === 'development' }
+    cookie: { secure: NODE_ENV === 'production' }
   }));
 
   
