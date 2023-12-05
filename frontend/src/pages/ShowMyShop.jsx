9f0d5b08ab700f08cfc4473d55161434295e0080
LakshyaRajoria <62789062+LakshyaRajoria@users.noreply.github.com>
2023-12-06 03:25:30 +0530

    Update ShowMyShop.jsx

--- frontend/src/pages/ShowMyShop.jsx
+++ frontend/src/pages/ShowMyShop.jsx
@@ -23,7 +23,7 @@ const ShowMyShop = () => {
 
     const handleLoad = async () => {
         try {
-            const auth = await axios.get('http://localhost:3000/authenticate', { withCredentials: true });
+            const auth = await axios.get('https://madeathome-backend2.onrender.com/authenticate', { withCredentials: true });
             console.log("we are on store menu page")
             if (auth.data.msg === 'authenticated') {
                 console.log('authenticated');
